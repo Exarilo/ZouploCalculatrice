fdf6dcc22c506c2f1db7ec4cdfd7cc241adcb5b8
Romain Madery <romain.madery92@gmail.com>
2021-04-30 17:09:36 +0200

    Operateur + et - qui marchent

--- app/src/main/java/com/example/zouplocalculatrice/MainActivity.java
+++ app/src/main/java/com/example/zouplocalculatrice/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     TextView tvResult;
     Button bt0;
@@ -204,6 +207,57 @@ public class MainActivity extends AppCompatActivity {
         return mot;
     }
     public void Calcul(){
+        List<Character>listChars = convertStringToCharList(tvResult.getText().toString());
+        List<String> listOperator =  new ArrayList<String>() ;
+        List<String> listCharNumber =  new ArrayList<String>() ;
+        List<String> listNumber =  new ArrayList<String>() ;
+        int indexOfOperator;
 
+        String RealNumber;
+        boolean isOperator=false;
+
+        for (int i = 0; i < listChars.size(); i++) {
+            if(listChars.get(i).toString().contentEquals("+")|| listChars.get(i).toString().contentEquals("-")|| listChars.get(i).toString().contentEquals("/")){
+                listOperator.add(listChars.get(i).toString());
+                isOperator=true;
+            }
+            else{
+                if(isOperator) {
+                    RealNumber="";
+                    for (int j = 0; j < listCharNumber.size(); j++) {
+                      RealNumber+= listCharNumber.get(j);
+                    }
+                    listNumber.add(RealNumber);
+                    listCharNumber.clear();
+                    isOperator=false;
+
+                }
+                listCharNumber.add(listChars.get(i).toString());
+            }
+        }
+        RealNumber="";
+        for (int j = 0; j < listCharNumber.size(); j++) {
+            RealNumber+= listCharNumber.get(j);
+        }
+        listNumber.add(RealNumber);
+        int res=Integer.parseInt(listNumber.get(0));
+        for (int i = 1; i < listNumber.size(); i++) {
+            if(listOperator.get(i-1).contentEquals("+"))
+                res+=Integer.parseInt(listNumber.get(i));
+            else if(listOperator.get(i-1).contentEquals("-"))
+                res-=Integer.parseInt(listNumber.get(i));
+            else if(listOperator.get(i-1).contentEquals("/"))
+                res/=Integer.parseInt(listNumber.get(i));
+        }
+        tvResult.setText(String.valueOf(res));
+    }
+
+    public static List<Character> convertStringToCharList(String str)
+    {
+        List<Character> chars = new ArrayList<>();
+        for (char ch : str.toCharArray()) {
+            chars.add(ch);
+        }
+        return chars;
     }
 }
